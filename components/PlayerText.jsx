931853f4627ae934e204c4adfe5ae7975f027b3b
Paulo Conde <pauloconde@gmail.com>
2024-08-29 12:51:36 -0400

    Stilized Player Text Dialog

--- components/PlayerText.jsx
+++ components/PlayerText.jsx
@@ -25,14 +25,14 @@ export function PlayerText({ playerName }) {
         <TouchableOpacity style={styles.button} onPress={showDialog}>
             <Text style={styles.playertext}>{name}</Text>
            
-            <Dialog.Container visible={visible}>
-                <Dialog.Title>Nombre del jugador</Dialog.Title>
-                <Dialog.Input 
+            <Dialog.Container visible={visible} contentStyle={styles.dialogContainer}>
+                <Dialog.Title style={styles.dialogTitle} >Nombre del jugador</Dialog.Title>
+                <Dialog.Input style={styles.dialogInput}
                   value={tempName}
                   onChangeText={(newName) => setTempName(newName)} // Actualizar solo el estado temporal
                 />
-                <Dialog.Button label="Cancelar" onPress={handleCancel} />
-                <Dialog.Button label="Guardar" onPress={handleSave} />
+                <Dialog.Button label="Cancelar" onPress={handleCancel} style={styles.dialogButton}/>
+                <Dialog.Button label="Guardar" onPress={handleSave} style={styles.dialogButton}/>
             </Dialog.Container>
         </TouchableOpacity>
     );
@@ -51,5 +51,23 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         minWidth: 250,
         alignItems: 'center',
+    },
+    dialogContainer: {
+      backgroundColor: '#555555', // Fondo del diálogo
+      borderRadius: 10, // Bordes redondeados
+      padding: 20, // Padding interno del diálogo
+    },
+    dialogTitle: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#fff', // Color del título
+    },
+    dialogInput: {
+      fontSize: 18,
+      fontWeight: '400',
+      color: '#fff', // Color del título
+    },
+    dialogButton: {
+      color: '#FFCC00', // Color del texto del botón
     },
 });
